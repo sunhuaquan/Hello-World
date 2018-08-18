efb38cf8f316784d55b5747258d5751e1bd81648
sunhuaquan <823045628@qq.com>
2018-08-18 11:42:25 +0800

    项目搭建

---
+++ src/com/sun/service/UserService.java
@@ -0,0 +1,9 @@
+package com.sun.service;
+
+import com.sun.domain.User;
+
+public interface UserService {
+
+
+     boolean isExist(User user);
+}
---
+++ src/com/sun/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.sun.service.impl;
+
+import com.sun.domain.User;
+import com.sun.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public boolean isExist(User user) {
+
+        return false;
+    }
+}
